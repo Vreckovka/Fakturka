27e4718d96014ff601215c85cb9d5d5df626e2d0
Roman Pecho <Roman Pecho@DESKTOP-BQAGF6F>
2024-03-03 15:12:47 +0100

    dsa

--- app/components/invoice-document.component.tsx
+++ app/components/invoice-document.component.tsx
@@ -18,11 +18,11 @@ Font.register({
 Font.register({
   family: "Open Sans",
   fonts: [
-    { src: "http://fonts.gstatic.com/s/opensans/v13/DXI1ORHCpsQm3Vp6mXoaTS3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 300 },
-    { src: "http://fonts.gstatic.com/s/opensans/v13/IgZJs4-7SA1XX_edsoXWog.ttf", fontWeight: 400 },
-    { src: "http://fonts.gstatic.com/s/poppins/v1/4WGKlFyjcmCFVl8pRsgZ9vesZW2xOQ-xsNqO47m55DA.ttf", fontWeight: 500 },
-    { src: "http://fonts.gstatic.com/s/opensans/v13/MTP_ySUJH_bn48VBG8sNSi3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 600 },
-    { src: "http://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzC3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 700 },
+    { src: "https://fonts.gstatic.com/s/opensans/v13/DXI1ORHCpsQm3Vp6mXoaTS3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 300 },
+    { src: "https://fonts.gstatic.com/s/opensans/v13/IgZJs4-7SA1XX_edsoXWog.ttf", fontWeight: 400 },
+    { src: "https://fonts.gstatic.com/s/poppins/v1/4WGKlFyjcmCFVl8pRsgZ9vesZW2xOQ-xsNqO47m55DA.ttf", fontWeight: 500 },
+    { src: "https://fonts.gstatic.com/s/opensans/v13/MTP_ySUJH_bn48VBG8sNSi3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 600 },
+    { src: "https://fonts.gstatic.com/s/opensans/v13/k3k702ZOKiLJc3WVjuplzC3USBnSvpkopQaUR-2r7iU.ttf", fontWeight: 700 },
   ],
 });
 
